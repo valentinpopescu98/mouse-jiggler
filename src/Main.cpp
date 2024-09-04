afbcf9f71eabd4bbc02f8462897c9b5866e22fe0
Valentin Popescu <Valentin.Popescu@mindit.io>
2024-09-04 13:07:16 +0300

    Added support for user input

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,8 +2,21 @@
 #include "Jiggler.h"
 
 int main() {
+    unsigned long long timeSecs;
+    unsigned long long timeStepMs;
+    int radLengthPix;
+
+    std::cout << "How much time should the application run (in seconds)? ";
+    std::cin >> timeSecs;
+
+    std::cout << "How often should the cursor position update (in miliseconds)? ";
+    std::cin >> timeStepMs;
+
+    std::cout << "How big should the radius of the circle around which the cursor will rotate (in pixels)? ";
+    std::cin >> radLengthPix;
+
     Jiggler jiggler;
-    jiggler.rotateCursor(10, 500, 10);
+    jiggler.rotateCursor(timeSecs, timeStepMs, radLengthPix);
 
     return 0;
 }
